11ebf7e2fcb7e190cefbdf8f00c6b0d046bbf9d7
hujiaoda <2840143759@qq.com>
2026-03-02 00:11:22 +0800

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<ranges>
+#include<algorithm>
 
+using namespace std;
+
+void oldone() {//传统写法
+    vector<int> v = { 5, 2, 9, 1 };
+    sort(v.begin(), v.end());           // 要写 .begin() .end()
+    auto it = find(v.begin(), v.end(), 9);
+    int maxVal = *max_element(v.begin(), v.end());
+}
+
+void newone() {//新的写法
+    vector<int> v = { 5, 2, 9, 1 };
+    ranges::sort(v);                     // 直接传 v 就行
+    auto it = ranges::find(v, 9);
+    int maxVal = ranges::max(v);          // 你用的就是这个！
+}
+
+int main() {
+    return 0;
+}
